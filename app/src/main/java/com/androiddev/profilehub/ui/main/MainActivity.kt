87c209df2ba98d4a72b00c37e0096fae63022c8e
NadyaNikol <nikolaenkonadin@gmail.com>
2025-04-30 19:17:02 +0300

    PH-3: added display user name and set profile photo in MainActivity.

--- app/src/main/java/com/androiddev/profilehub/ui/main/MainActivity.kt
+++ app/src/main/java/com/androiddev/profilehub/ui/main/MainActivity.kt
@@ -12,12 +12,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val userName: String = intent.getStringExtra(EXTRA_USER_NAME).orEmpty()
+        binding.tvNameProfile.text = userName
+        binding.ivPhotoProfile.setImageResource(R.drawable.user_photo)
     }
 }
